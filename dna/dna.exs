a3e852df35923da982cd194b9491fc9e4be9ef3a
Duponin <pwet@dupon.in>
2020-03-26 22:27:38 +0100

    Add dna.exs

---
+++ dna/dna.exs
@@ -0,0 +1,36 @@
+#! /usr/bin/env elixir
+
+defmodule DNA do
+  def complement(acid) do
+    case acid do
+      "A" -> "T"
+      "C" -> "G"
+      "T" -> "A"
+      "G" -> "C"
+    end
+  end
+
+  def randomizer do
+    case :rand.uniform(4) do
+      1 -> "A"
+      2 -> "C"
+      3 -> "T"
+      4 -> "G"
+    end
+  end
+
+  def generate_chain(repetition) do
+    Enum.map(1..repetition, fn _ ->
+      base = DNA.randomizer()
+      base_complement = base |> DNA.complement()
+      IO.puts("#{base} —— #{base_complement}")
+    end)
+  end
+end
+
+{arguments, _, _} =
+  System.argv()
+  |> OptionParser.parse(strict: [repeat: :integer])
+
+arguments[:repeat]
+|> DNA.generate_chain()
