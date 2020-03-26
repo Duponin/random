07dea1bdbcbcda22e43bcb311e4450e91a349a3b
Duponin <pwet@dupon.in>
2020-03-26 21:00:46 +0100

    Add dna.sh

---
+++ dna/dna.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+set -e
+
+function complement(){
+    case $1 in
+       "A")
+           echo "T"
+           ;;
+       "C")
+           echo "G"
+           ;;
+       "T")
+           echo "A"
+           ;;
+       "G")
+           echo "C"
+           ;;
+   esac
+}
+
+function randomizer(){
+    RANDOM_VALUE=$(($RANDOM % 3))
+    case $RANDOM_VALUE in
+        0)
+            echo "A"
+            ;;
+        1)
+            echo "C"
+            ;;
+        2)
+            echo "T"
+            ;;
+        3)
+            echo "G"
+            ;;
+    esac
+}
+
+for i in $(seq 1 $1); do
+    PAIR_1="$(randomizer)"
+    PAIR_2="$(complement $PAIR_1)"
+
+    printf "%s —— %s\n" "$PAIR_1" "$PAIR_2"
+done
